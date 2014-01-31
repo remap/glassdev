cc45dfad5d198334aadf61295502d87e14e6205c
Alex Nano <alexnano@ucla.edu>
2014-01-30 19:25:01 -0800

    cleanup prior to merging w/ video recording

--- GlassChat/android/glasschat/src/com/remap/glass/GlassChat.java
+++ GlassChat/android/glasschat/src/com/remap/glass/GlassChat.java
@@ -2,34 +2,47 @@ package com.remap.glass;
 import android.provider.Settings.Secure;
 
 import android.app.Activity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * @author Alex Nano
  */
 public class GlassChat extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+	private static final String URL = "http://ether.remap.ucla.edu/glass/index.html?uid=";
+	
+	// these are for the media player
+	MediaRecorder recorder;
+	SurfaceHolder holder;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // get glass UID
         String android_id = Secure.getString(this.getContentResolver(),Secure.ANDROID_ID); 
+        
+        // initalize webkit view
         WebView engine = (WebView) findViewById(R.id.web_engine);
-        engine.setWebChromeClient(chromeClient);
-        engine.setWebViewClient(wvClient);
+        
+        
+        // this is an attempt at getting HTML5 video to play
+        // turns out it's far more complicated... yet leaving
+        WebChromeClient chromeClient = null;
+		engine.setWebChromeClient(chromeClient);
+        WebViewClient wvClient = null;
+		engine.setWebViewClient(wvClient);
         engine.getSettings().setJavaScriptEnabled(true);
-        engine.getSettings().setPluginsEnabled(true)
-        engine.loadUrl("http://ether.remap.ucla.edu/glass/index.html?uid="+android_id);
+        engine.getSettings().setPluginsEnabled(true);
+        
+        // load the page for display on glass
+        engine.loadUrl(URL+android_id);
        
-
-        //String data = "<html>" + 
-        //"<meta name='viewport content=width=device-width, initial-scale=1.02' />" +
-         //       "<body><h1>Yay, Glass Chat+!</h1></body>" +
-           //     "</html>";
-
-        //engine.loadData(android_id, "text/html", "UTF-8");
+        
     }
 }
